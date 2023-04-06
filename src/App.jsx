41fb7219b69c8eb713e7850095d82fcf117538b0
midCenturyDude7 <mgriffes@gmail.com>
2023-04-05 21:24:49 -0700

    Initial commit | project structure/org

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+
+function App() {
+  return (
+    <main className="column">
+      <h1>Auth0 Login</h1>
+      <LoginButton />
+      <LogoutButton />
+    </main>
+  );
+}
+
+export default App;
